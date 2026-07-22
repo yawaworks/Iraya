c026ec51c04ecf44a402f41ba190c306dad9c803
indeedyashika <indeedyashika@gmail.com>
2026-07-22 06:40:21 +0530

    Refactor next.config.js to simplify allowedDevOrigins configuration

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ["192.168.1.36"],
-  },
+  allowedDevOrigins: ["192.168.1.36"],
 };
 
 export default nextConfig;
